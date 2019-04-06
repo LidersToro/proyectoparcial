0abec60b004b3f2f85f7605662ee074c477b839a
Liders Toro <liderz250@gmail.com>
2019-04-06 00:34:08 -0400

    primer commit al mvc

---
+++ proyectoprueba/MVCprueba/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCprueba.Startup))]
+namespace MVCprueba
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
