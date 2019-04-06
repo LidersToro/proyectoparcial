242ea0b5ba13801d98d1986c6ec1020daabf0808
Liders Toro <liderz250@gmail.com>
2019-04-06 00:45:38 -0400

    modelo MVC

---
+++ proyectoprueba/MVCprueba/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVCprueba.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<MVCprueba.Models.Product> Products { get; set; }
+    }
+}
